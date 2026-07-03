341ad8b8d56541abedb1c3895d35cc1e5f87e5dd
lucacondo <rojasgonzaloomar@gmail.com>
2026-07-02 21:05:32 -0300

    Se creo el modelo Movie con las propiedades pedidas

---
+++ src/models/movie.model.js
@@ -0,0 +1,33 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database.js";
+import { type } from "node:os";
+import { timeStamp } from "node:console";
+
+export const movies = sequelize.define (
+    'Movie',
+    {
+        title:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        },
+        genre:{
+            type:DataTypes.STRING,
+            allowNull:false
+        },
+        duration:{
+            type:DataTypes.INTEGER,
+            allowNull:false
+        },
+        year:{
+            type:DataTypes.INTEGER,
+            allowNull:false
+        },
+        synopsis:{
+            type:DataTypes.TEXT,
+            allowNull:true
+        }
+    },
+    {
+        timestams:false,
+    },
+);
